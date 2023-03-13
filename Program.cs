7db0be45b90f159bf645eed184513bcaf409ca12
PedroRC547 <candianipedro547@gmail.com>
2023-03-12 21:55:28 -0300

    Aplicação que envia informações para uma API

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Net.Http.Headers;
+
+namespace UtilizandoAPI
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://localhost:7078/api/Log"))
+                {
+                    request.Headers.TryAddWithoutValidation("accept", "text/plain");
+
+                    request.Content = new StringContent("{\n\n  \"mensagem\": \"{teset}\",\n  \"data_criacao\": 12032023\n\n}");
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                    var response = await httpClient.SendAsync(request);
+                }
+            }
+        }
+
+    }
+}
