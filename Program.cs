9e858cb4c83db9c4c4912f97a4399b98f1d6471f
Pedro Rodrigues Candiani <53916509+PedroRC547@users.noreply.github.com>
2023-03-15 22:57:14 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,16 +7,22 @@ namespace UtilizandoAPI
     {
         public static async Task Main(string[] args)
         {
-            using (var httpClient = new HttpClient())
+            Console.WriteLine("Quantidade de Log random: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < x; i++)
             {
-                using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://localhost:7078/api/Log"))
+                using (var httpClient = new HttpClient())
                 {
-                    request.Headers.TryAddWithoutValidation("accept", "text/plain");
+                    using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://localhost:7078/api/Log"))
+                    {
+                        request.Headers.TryAddWithoutValidation("accept", "text/plain");
 
-                    request.Content = new StringContent("{\n\n  \"mensagem\": \"{teset}\",\n  \"data_criacao\": 12032023\n\n}");
-                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+                        request.Content = new StringContent("{\n\n  \"mensagem\": \"{teset}\",\n  \"data_criacao\": 12032023\n\n}");
+                        request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
 
-                    var response = await httpClient.SendAsync(request);
+                        var response = await httpClient.SendAsync(request);
+                    }
                 }
             }
         }
